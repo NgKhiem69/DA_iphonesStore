4209dd0379be40b546cd5d0a4bd13971b239b7a2
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-27 21:36:37 +0700

    Update CategoryRepository.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/repository/CategoryRepository.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/repository/CategoryRepository.java
@@ -1,5 +1,9 @@
 package com.fit.ntu.electronics.repository;
 
-public class CategoryRepository {
+import com.fit.ntu.electronics.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }

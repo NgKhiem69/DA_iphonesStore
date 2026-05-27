416ba299e85cde1e603cc94f58ae8f72976ab481
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-28 05:26:14 +0700

    UPDATE productRepository

--- electronic-store/src/main/java/com/fit/ntu/electronics/repository/ProductRepository.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/repository/ProductRepository.java
@@ -1,11 +1,12 @@
 package com.fit.ntu.electronics.repository;
 
 import com.fit.ntu.electronics.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCategoryId(Long categoryId);
+    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
 }

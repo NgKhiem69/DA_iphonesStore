0526253f56dff81b461a6661708cea36f3aefc87
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-27 21:38:08 +0700

    Update FavoriteRepository.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/repository/FavoriteRepository.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/repository/FavoriteRepository.java
@@ -1,5 +1,11 @@
 package com.fit.ntu.electronics.repository;
 
-public class FavoriteRepository {
+import com.fit.ntu.electronics.model.Favorite;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+    List<Favorite> findByUserId(Long userId);
 }

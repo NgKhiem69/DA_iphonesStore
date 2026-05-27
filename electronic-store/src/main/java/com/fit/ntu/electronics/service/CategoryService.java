9725ecc9298e230cda7e36a8df9103f372f39edf
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-27 21:40:25 +0700

    Update CategoryService.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/service/CategoryService.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/service/CategoryService.java
@@ -1,5 +1,32 @@
 package com.fit.ntu.electronics.service;
 
+import com.fit.ntu.electronics.model.Category;
+import com.fit.ntu.electronics.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CategoryService {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category getCategoryById(Long id) {
+        Optional<Category> optional = categoryRepository.findById(id);
+        return optional.orElse(null);
+    }
+
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
 }

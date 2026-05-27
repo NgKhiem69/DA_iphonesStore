cf6bcd1f4c82dff8ce28de0840e4579b3be3c511
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-28 01:51:07 +0700

    Update WishlistItemRepository.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/repository/WishlistItemRepository.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/repository/WishlistItemRepository.java
@@ -1,5 +1,12 @@
 package com.fit.ntu.electronics.repository;
 
-public class WishlistItemRepository {
+import com.fit.ntu.electronics.model.WishlistItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
+public interface WishlistItemRepository extends JpaRepository<WishlistItem, Long> {
+    List<WishlistItem> findByUserId(Long userId);
+    WishlistItem findByUserIdAndProductId(Long userId, Long productId);
 }

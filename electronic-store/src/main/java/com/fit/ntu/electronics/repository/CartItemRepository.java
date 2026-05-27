affc8ccf65f12779158dba4906cee6caf808b804
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-27 21:38:34 +0700

    Update CartItemRepository.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/repository/CartItemRepository.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/repository/CartItemRepository.java
@@ -1,5 +1,12 @@
 package com.fit.ntu.electronics.repository;
 
-public class CartItemRepository {
+import com.fit.ntu.electronics.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByUserId(Long userId);
+    CartItem findByUserIdAndProductId(Long userId, Long productId);
 }

16570d59fc97d63e60182eeafc38fd8b8b566d02
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-27 21:40:56 +0700

    Update ProductService.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/service/ProductService.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/service/ProductService.java
@@ -1,5 +1,36 @@
 package com.fit.ntu.electronics.service;
 
+import com.fit.ntu.electronics.model.Product;
+import com.fit.ntu.electronics.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProductService {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public List<Product> getProductsByCategoryId(Long categoryId) {
+        return productRepository.findByCategoryId(categoryId);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product getProductById(Long id) {
+        Optional<Product> optional = productRepository.findById(id);
+        return optional.orElse(null);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
 }

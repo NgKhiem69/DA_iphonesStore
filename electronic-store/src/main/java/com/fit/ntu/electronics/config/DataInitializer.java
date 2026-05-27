ec83cde7ad777a8abb606aa3e2da1a0541ef3ef0
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-28 03:07:14 +0700

    Update DataInitializer.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/config/DataInitializer.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/config/DataInitializer.java
@@ -1,5 +1,35 @@
 package com.fit.ntu.electronics.config;
 
-public class DataInitializer {
+import com.fit.ntu.electronics.model.User;
+import com.fit.ntu.electronics.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (userRepository.count() == 0) {
+            User admin = new User();
+            admin.setEmail("admin@gmail.com");
+            admin.setPassword("admin123");
+            admin.setFirstName("Admin");
+            admin.setLastName("System");
+            admin.setRole("ADMIN");
+            userRepository.save(admin);
+
+            User user = new User();
+            user.setEmail("user@gmail.com");
+            user.setPassword("user123");
+            user.setFirstName("Md");
+            user.setLastName("Rimel");
+            user.setRole("USER");
+            userRepository.save(user);
+        }
+    }
 }

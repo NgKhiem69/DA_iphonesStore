51cc75d434d49b04367a8ac8d2ad342e5e5b586f
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-27 18:58:43 +0700

    Update Favorite.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/model/Favorite.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/model/Favorite.java
@@ -1,5 +1,31 @@
 package com.fit.ntu.electronics.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "favorites")
 public class Favorite {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Favorite() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public Product getProduct() { return product; }
+    public void setProduct(Product product) { this.product = product; }
 }

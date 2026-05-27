c5a048ed5e484abf326d022015fd7a23a0fbee23
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-27 21:37:10 +0700

    Update UserRepository.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/repository/UserRepository.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.fit.ntu.electronics.repository;
 
-public class UserRepository {
+import com.fit.ntu.electronics.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }

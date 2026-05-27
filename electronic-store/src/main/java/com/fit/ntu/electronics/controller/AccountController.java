1462b4389c942658dd80b3f882f318fa5dc344de
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-28 02:12:01 +0700

    Update AccountController.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/controller/AccountController.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/controller/AccountController.java
@@ -1,5 +1,22 @@
 package com.fit.ntu.electronics.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/profile")
 public class AccountController {
 
+    @GetMapping
+    public String viewProfile(Model model) {
+        // Truyền tạm thông tin người dùng mẫu ra view
+        model.addAttribute("firstName", "Md");
+        model.addAttribute("lastName", "Rimel");
+        model.addAttribute("email", "rimel1111@gmail.com");
+        model.addAttribute("address", "Kingston, 5236, United State");
+        
+        return "profile";
+    }
 }

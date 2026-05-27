ed2e276af6ce055cac839176efc0a5c9e12cff5d
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-27 18:59:20 +0700

    Update CartItem.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/model/CartItem.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/model/CartItem.java
@@ -1,5 +1,36 @@
 package com.fit.ntu.electronics.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cart_items")
 public class CartItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public CartItem() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Integer getQuantity() { return quantity; }
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public Product getProduct() { return product; }
+    public void setProduct(Product product) { this.product = product; }
 }

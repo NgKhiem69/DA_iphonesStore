2b3c27ca4c26c0554c9a1d400115f1d29a6b3ddb
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-28 00:45:57 +0700

    Update EmailService.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/service/EmailService.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/service/EmailService.java
@@ -1,5 +1,29 @@
 package com.fit.ntu.electronics.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
 public class EmailService {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendOrderEmail(String toEmail, String emailContent) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            
+            helper.setTo(toEmail);
+            helper.setSubject("Xác nhận đơn hàng thành công từ Exclusive Store");
+            helper.setText(emailContent, true);
+            
+            mailSender.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

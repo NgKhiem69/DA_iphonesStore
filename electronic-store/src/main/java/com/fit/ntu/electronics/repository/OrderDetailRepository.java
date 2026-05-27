a24b10a88df0a2f992243111a36e0192c2324d12
Nguyễn Gia Khiêm <gk699488@gmail.com>
2026-05-27 21:39:39 +0700

    Update OrderDetailRepository.java

--- electronic-store/src/main/java/com/fit/ntu/electronics/repository/OrderDetailRepository.java
+++ electronic-store/src/main/java/com/fit/ntu/electronics/repository/OrderDetailRepository.java
@@ -1,5 +1,11 @@
 package com.fit.ntu.electronics.repository;
 
-public class OrderDetailRepository {
+import com.fit.ntu.electronics.model.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+    List<OrderDetail> findByOrderId(Long orderId);
 }
